747401c1513b988ab320cbd0fa7323ea2e21e5c0
harsha nivas <224g1a0525@srit.ac.in>
2026-04-08 16:04:52 +0530

    converted to static structure

--- poject code/app.py
+++ poject code/app.py
@@ -212,4 +212,4 @@ def prediction():
     return render_template('prediction.html', sentiment="", drug_name="", condition="", recommended_drugs=[], msg="")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
